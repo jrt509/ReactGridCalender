4ecf30389015a969582da5e86d79fde14b2acb90
Mac1 <bischi8@gmail.com>
2020-06-18 11:12:22 -0700

    initial commit

---
+++ src/components/content/content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DaysWrapper from "./daysWrapper"
+
+export default function contentWrapper(props) {
+    return (
+        <div className="content-wrapper">
+            <DaysWrapper />
+        </div>
+    )
+}
