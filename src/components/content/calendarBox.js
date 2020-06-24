480a6f17909fa829c6c18a155f5d5fe0e858a58a
Mac1 <bischi8@gmail.com>
2020-06-24 09:14:32 -0700

    fixed events bug

--- src/components/content/calendarBox.js
+++ src/components/content/calendarBox.js
@@ -8,17 +8,53 @@ export default class CalendarBox extends Component {
         this.state = {
             text: ""
         }
+        this.handleChange =this.handleChange.bind(this)
     }
     componentDidMount() {
         if (!this.props.overflow) {
-            const { date, month, year} = this.props
-            fetch(`http://127.0.0.1:5000/reminder/get/${date}/${month}/${year}`, { method: "GET"})
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ text: data.text})
-            })
-            .catch(error => console.log(error))
+           this.getReminderData()
         }
+    }
+    componentDidUpdate(prevProps) {
+        if (prevProps.month !== this.props.month){
+            if (!this.props.overflow) {
+                this.getReminderData()
+            }
+        }
+    }
+
+    getReminderData() {
+        const { date, month, year} = this.props
+        fetch(`http://127.0.0.1:5000/reminder/get/${date}/${month}/${year}`, { method: "GET"})
+        .then(response => response.json())
+        .then(data => {
+            if(data.text) {
+                this.setState({ text: data.text })
+            }
+            else {
+                this.setState({ text: ""})
+            }
+        })
+        .catch(error => console.log(error))
+    }
+
+    handleChange(event) {
+        this.setState({ text: event.target.value })
+    }
+    handleSubmit() {
+        fetch("http://127.0.0.1:5000/reminder/add", {
+            method: "POST",
+            headers: { "content-type": "application/json"},
+            body: JSON.stringify({ 
+                text: this.state.text,
+                date: this.props.date,
+                month: this.props.month,
+                year: this.props.year
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
     }
     render() {
         return (
@@ -28,7 +64,10 @@ export default class CalendarBox extends Component {
                 </div>
                 <textarea
                     disabled={this.props.overflow} 
-                    value={this.state.text}></textarea>
+                    value={this.state.text}
+                    onChange={this.handleChange}
+                    onBlur={this.handleSubmit}
+                    ></textarea>
             </div>
         )
     }
